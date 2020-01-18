1d6952a6036c8db250e52b12d386c5649de5a586
salim <salim057@live.com>
2020-01-18 11:00:06 +0000

    Eureka server for Eatza

--- orderingservice/src/main/java/com/eatza/order/OrderingserviceApplication.java
+++ orderingservice/src/main/java/com/eatza/order/OrderingserviceApplication.java
@@ -3,10 +3,12 @@ package com.eatza.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.eatza.order.config.JwtFilter;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderingserviceApplication {
 
--- restaurantsearchservice/src/main/java/com/eatza/restaurantsearch/RestaurantsearchserviceApplication.java
+++ restaurantsearchservice/src/main/java/com/eatza/restaurantsearch/RestaurantsearchserviceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.eatza.restaurantsearch.config.JwtFilter;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableCaching
 public class RestaurantsearchserviceApplication {
